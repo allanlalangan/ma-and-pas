0e1e70657beee7afca7533b82bab3feec338c06e
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-12-02 23:54:34 -0800

    feat: extend tailwind theme with fontFamily update body font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
   content: ['pages/*.{js,ts,jsx,tsx}', 'components/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        chivo: ['"Chivo"', 'sans-serif'],
+        heading: ['"Taviraj"', 'serif'],
+        body: ['"Didact Gothic"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
