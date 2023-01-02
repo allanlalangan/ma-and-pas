18dac84d1484c883528522e4863b2ead7b3e73e7
allanlalangan <allanlalangan@gmail.com>
2023-01-01 21:49:16 -0800

    build: add font, update all fonts and tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["pages/*.{js,ts,jsx,tsx}", "components/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        chivo: ['"Playfair Display"', "sans-serif"],
-        heading: ['"Roboto Mono"', "sans-serif"],
-        body: ['"Rasa"', "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["pages/*.{js,ts,jsx,tsx}", "components/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				display: ['"Avango"', "serif"],
+				heading: ['"Playfair Display"', "sans-serif"],
+				body: ['"Rasa"', "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
