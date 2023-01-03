4665b29ee964b70243fee8370fe63893c2ac3a57
allanlalangan <allanlalangan@gmail.com>
2023-01-02 22:33:38 -0800

    feat: update header nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,33 @@
+import Link from "next/link";
+
 const Header = () => {
 	const title: string = "Mom & Pa's";
 	return (
-		<header className="flex w-full flex-col justify-center p-4">
-			<h1 className="font-display text-2xl font-bold text-green-600">
+		<header className="flex w-full justify-center bg-amber-50 p-4">
+			<h1 className="w-1/3 font-display text-2xl font-bold text-emerald-700">
 				{title}
 			</h1>
-			<nav className="">
-				<section className="m-auto flex justify-center">
-					<button className="mr-4">Chairs & Seating</button>
-					<button className="mr-4">Kitchen</button>
-					<button className="mr-4">Storage</button>
-					<button className="mr-4">Electronics</button>
+			<nav className="flex w-full items-center justify-evenly">
+				<section className="flex w-full justify-center text-stone-800">
+					<Link href="/" className="mx-4">
+						Chairs & Seating
+					</Link>
+					<Link href="/" className="mx-4">
+						Kitchen
+					</Link>
+					<Link href="/" className="mx-4">
+						Storage
+					</Link>
+					<Link href="/" className="mx-4">
+						Electronics
+					</Link>
 				</section>
-				<section className="flex w-full justify-between">
-					<button className="rounded-lg border border-amber-100 p-2 px-8 text-orange-50">
-						Menu
-					</button>
-					<button className="rounded-lg border border-amber-100 p-2 px-8 text-orange-50">
+				<section className="flex justify-evenly">
+					<button className="border-b-2 border-stone-900 p-2 px-8 text-stone-800">
 						Cart
+					</button>
+					<button className="border-b-2 border-stone-800 p-2 px-8 text-stone-800">
+						Login
 					</button>
 				</section>
 			</nav>
