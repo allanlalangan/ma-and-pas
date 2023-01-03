5c92750342d2a4b4ee8f11a55337f59b8c3059aa
allanlalangan <allanlalangan@gmail.com>
2023-01-02 22:33:27 -0800

    feat: update global colors

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
 	return (
-		<section className="flex min-h-[80vh] flex-col items-start justify-center bg-stone-800 p-4">
+		<section className="flex min-h-[80vh] flex-col items-start justify-center bg-amber-50 p-4 text-stone-800">
 			<p className="font-display text-4xl">
 				Unique pieces with a familiar charm
 			</p>
--- components/NewArrivals.tsx
+++ components/NewArrivals.tsx
@@ -1,7 +1,9 @@
 const NewArrivals = () => {
 	return (
 		<section className="flex h-[50vh] flex-col">
-			<h2 className="p-2 font-heading text-xl font-semibold">New Arrivals</h2>
+			<h2 className="p-2 font-heading text-xl font-semibold">
+				Our Latest Finds
+			</h2>
 			<ul className="grid h-full grid-cols-12 p-4">
 				<li className="col-span-12 m-4 rounded border border-amber-200 sm:col-span-6 md:col-span-3">
 					<h3 className="p-2">Title</h3>
