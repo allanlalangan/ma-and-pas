525e6b9507bd8254425b79d70679277164c40381
allanlalangan <allanlalangan@gmail.com>
2023-01-25 18:13:38 -0800

    feat: update Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,25 @@
+import Image from 'next/image';
+import heroImage from '../public/assets/rabie-madaci-skx1Rn6LW9I-unsplash.jpg';
+
 const Hero = () => {
 	return (
-		<section className="flex min-h-[80vh] flex-col items-start justify-center bg-amber-50 p-4 text-stone-800">
-			<p className="font-display text-4xl">
-				Unique pieces with a familiar charm
-			</p>
-			<p className="font-display text-4xl">collected over the years</p>
+		<section className='bg-amber-50 text-stone-800 lg:flex'>
+			<article className='w-1/2'>
+				<h1 className='font-display text-8xl'>
+					MOM
+					<br />& PA&apos;s
+				</h1>
+				<p className='w-1/2'>
+					an assortment of unique peices collected over the years
+				</p>
+			</article>
+			<figure className='flex flex-row-reverse lg:w-1/2'>
+				<Image
+					className='h-96 object-cover lg:h-[100vh]'
+					src={heroImage}
+					alt='vintage furniture'
+				/>
+			</figure>
 		</section>
 	);
 };
