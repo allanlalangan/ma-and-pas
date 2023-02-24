32eaf374071aafb5311eb1a4cd9092b7678f9272
allanlalangan <allanlalangan@gmail.com>
2023-02-23 20:13:16 -0800

    fix Hero heading z-index

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,10 +4,11 @@ import heroImage from '../public/assets/rabie-madaci-skx1Rn6LW9I-unsplash.jpg';
 const Hero = () => {
 	return (
 		<section className='lg:flex'>
-			<article className='mt-4 lg:w-1/3'>
-				<h1 className='font-brand text-8xl text-teal-900'>
-					MOM{` `}
-					<br className='hidden lg:block' />& PA&apos;s
+			<article className='mt-4 flex flex-col lg:w-1/3'>
+				<h1 className='-z-10 font-brand text-8xl text-teal-900'>
+					<span>MOM </span>
+					<br className='hidden lg:block' />
+					<span>& PA&apos;s</span>
 				</h1>
 				<p className='w-full text-xl'>
 					an assortment of unique peices collected over the years
