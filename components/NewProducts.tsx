2a7a7f390738116d468b8e03f73885b600f41248
allanlalangan <allanlalangan@gmail.com>
2023-02-23 20:47:59 -0800

    rename NewArrivals to NewProducts add newProduct description update NewProducts styles

---
+++ components/NewProducts.tsx
@@ -0,0 +1,50 @@
+const NewProducts = () => {
+	return (
+		<section className='flex flex-col bg-stone-900 text-amber-50'>
+			<h2 className='font-heading p-2 text-2xl font-semibold'>Latest Finds</h2>
+			<ul className='grid h-full grid-cols-12 gap-4 p-4'>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+					<h3 className='p-2'>Title</h3>
+					<p className='mb-4 flex-1 px-4'>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
+						fugiat a! Facilis.
+					</p>
+					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
+						<figcaption>img</figcaption>
+					</figure>
+				</li>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+					<h3 className='p-2'>Title</h3>
+					<p className='mb-4 flex-1 px-4'>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
+						fugiat a! Facilis.
+					</p>
+					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
+						<figcaption>img</figcaption>
+					</figure>
+				</li>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+					<h3 className='p-2'>Title</h3>
+					<p className='mb-4 flex-1 px-4'>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
+						fugiat a! Facilis.
+					</p>
+					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
+						<figcaption>img</figcaption>
+					</figure>
+				</li>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+					<h3 className='p-2'>Title</h3>
+					<p className='mb-4 flex-1 px-4'>
+						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
+						fugiat a! Facilis.
+					</p>
+					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
+						<figcaption>img</figcaption>
+					</figure>
+				</li>
+			</ul>
+		</section>
+	);
+};
+export default NewProducts;
