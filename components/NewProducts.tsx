215b634f5f9b047f82199d8dac842e1c5ead71ce
allanlalangan <allanlalangan@gmail.com>
2023-02-23 21:04:07 -0800

    update NewProducts responsive styles

--- components/NewProducts.tsx
+++ components/NewProducts.tsx
@@ -3,7 +3,7 @@ const NewProducts = () => {
 		<section className='flex flex-col bg-stone-900 text-amber-50'>
 			<h2 className='font-heading p-2 text-2xl font-semibold'>Latest Finds</h2>
 			<ul className='grid h-full grid-cols-12 gap-4 p-4'>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
 					<h3 className='p-2'>Title</h3>
 					<p className='mb-4 flex-1 px-4'>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
@@ -13,7 +13,7 @@ const NewProducts = () => {
 						<figcaption>img</figcaption>
 					</figure>
 				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
 					<h3 className='p-2'>Title</h3>
 					<p className='mb-4 flex-1 px-4'>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
@@ -23,7 +23,7 @@ const NewProducts = () => {
 						<figcaption>img</figcaption>
 					</figure>
 				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
 					<h3 className='p-2'>Title</h3>
 					<p className='mb-4 flex-1 px-4'>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
@@ -33,7 +33,7 @@ const NewProducts = () => {
 						<figcaption>img</figcaption>
 					</figure>
 				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 sm:col-span-6 lg:col-span-3'>
+				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
 					<h3 className='p-2'>Title</h3>
 					<p className='mb-4 flex-1 px-4'>
 						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
