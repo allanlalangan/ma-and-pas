1f81274929153ac2002297f74275ae166232407f
allanlalangan <allanlalangan@gmail.com>
2023-02-28 13:27:19 -0800

    update NewProducts map dummy data

--- components/NewProducts.tsx
+++ components/NewProducts.tsx
@@ -1,48 +1,28 @@
+import Image from 'next/image';
+import { products } from '../public/data';
+
 const NewProducts = () => {
+	console.log(products);
 	return (
 		<section className='flex flex-col bg-stone-900 text-amber-50'>
 			<h2 className='font-heading p-2 text-2xl font-semibold'>Latest Finds</h2>
 			<ul className='grid h-full grid-cols-12 gap-4 p-4'>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
-					<h3 className='p-2'>Title</h3>
-					<p className='mb-4 flex-1 px-4'>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
-						fugiat a! Facilis.
-					</p>
-					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
-						<figcaption>img</figcaption>
-					</figure>
-				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
-					<h3 className='p-2'>Title</h3>
-					<p className='mb-4 flex-1 px-4'>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
-						fugiat a! Facilis.
-					</p>
-					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
-						<figcaption>img</figcaption>
-					</figure>
-				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
-					<h3 className='p-2'>Title</h3>
-					<p className='mb-4 flex-1 px-4'>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
-						fugiat a! Facilis.
-					</p>
-					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
-						<figcaption>img</figcaption>
-					</figure>
-				</li>
-				<li className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'>
-					<h3 className='p-2'>Title</h3>
-					<p className='mb-4 flex-1 px-4'>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo odio
-						fugiat a! Facilis.
-					</p>
-					<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
-						<figcaption>img</figcaption>
-					</figure>
-				</li>
+				{products?.slice(0, 4).map((product, i) => (
+					<li
+						key={i}
+						className='col-span-12 flex flex-col rounded bg-stone-800 md:col-span-6 xl:col-span-3'
+					>
+						<h3 className='p-2'>{product.name}</h3>
+						<p className='mb-4 flex-1 px-4'>{product.description}</p>
+						<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
+							<Image
+								className='h-[50vh] object-cover'
+								src={product.image}
+								alt=''
+							/>
+						</figure>
+					</li>
+				))}
 			</ul>
 		</section>
 	);
