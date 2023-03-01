2cf3d4383c4d69f01d140a5b5b0dcb9fc9c350a6
allanlalangan <allanlalangan@gmail.com>
2023-02-28 18:33:27 -0800

    add individual product links to NewProducts

--- components/NewProducts.tsx
+++ components/NewProducts.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { products } from '../public/data';
 
 const NewProducts = () => {
@@ -14,6 +15,9 @@ const NewProducts = () => {
 					>
 						<h3 className='p-2'>{product.name}</h3>
 						<p className='mb-4 flex-1 px-4'>{product.description}</p>
+						<Link className='w-fit px-4' href={`/products/${i}`}>
+							View Product
+						</Link>
 						<figure className='min-h-[50vh] flex-1 rounded-b bg-gradient-to-t from-stone-700 to-stone-800'>
 							<Image
 								className='h-[50vh] object-cover'
