8c194eae5a26a1d8e8889d01ded3ba846954e190
allanlalangan <allanlalangan@gmail.com>
2023-04-01 16:46:06 -0700

    update hero image mosaic

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,39 @@
 import Image from 'next/image';
 import heroImage from '../public/assets/roman-kraft-ChSJFIAEoRM-unsplash.jpg';
+import heroImage2 from '../public/assets/rabie-madaci-skx1Rn6LW9I-unsplash.jpg';
 
 const Hero = () => {
 	return (
-		<section className='min-h-screen lg:flex'>
-			<article className='flex flex-col p-4 pt-4 lg:w-1/3'>
-				<h1 className='-z-10 font-brand text-8xl text-teal-900'>
-					<span>MOM </span>
+		<section className='min-h-screen bg-stone-900 text-amber-50 lg:flex lg:flex-col'>
+			<article className='flex flex-col p-4 pt-4 md:w-1/3 lg:w-full'>
+				<h1 className='-z-1 font-brand text-8xl uppercase text-amber-500'>
+					Curated charm
+					<br />
+					for your space
+					{/* <span>MOM </span>
 					<br className='hidden lg:block' />
-					<span>& PA&apos;s</span>
+					<span>& PA&apos;s</span> */}
 				</h1>
 				<p className='w-full text-xl'>
-					an assortment of unique peices collected over the years
+					An assortment of unique peices collected over the years
 				</p>
 			</article>
-			<figure className='lg:w-2/3'>
+			<figure className='relative flex px-12 pb-12 md:w-2/3 lg:w-full'>
 				<Image
-					className='object-cover lg:h-screen'
+					className='object-cover lg:h-[75vh] lg:w-2/3'
 					src={heroImage}
 					alt='vintage furniture'
 				/>
+				<Image
+					className='absolute right-12 top-96 w-1/3 object-cover'
+					src={heroImage2}
+					alt='vintage furniture'
+				/>
+				<figcaption className='w-1/3 p-4 text-xl'>
+					We collect many things. From vintage apparel, to haunted furniture.
+					Many of our peices have a story to tell; and many don&apos;t, but they
+					would look just as charming in a loving home.
+				</figcaption>
 			</figure>
 		</section>
 	);
